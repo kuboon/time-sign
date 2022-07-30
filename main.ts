9919c08f38e5fecde8c98a41236fa131880c18a4
Ohkubo KOHEI <kuboon@trick-with.net>
2022-07-30 00:50:04 +0000

    deno deploy

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
-// 1.Wasmをロード
-const wasmCode = await Deno.readFile("./ibe-wasm/target/wasm32-unknown-unknown/debug/ibe_wasm.wasm");
-// 2.ロードしたファイルからWasmモジュールを作成
-const wasmModule = new WebAssembly.Module(wasmCode);
-// 3.関数を使用できるようモジュールのインスタンスを作成
-const wasmInstance = new WebAssembly.Instance(wasmModule);
+import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+import { instantiate } from "./lib/rs_lib.generated.js";
 
-const { square } = wasmInstance.exports;
+const { add } = await instantiate();
 
-console.log(square(1)); // 1
-console.log(square(2)); // 4
-console.log(square(3)); // 9
-console.log(square(4)); // 16
+serve((_req: Request) => new Response(add(1, 2).toString()));
