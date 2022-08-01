257633d74b09810b99ce168269f604e727a961fd
Ohkubo KOHEI <kuboon@trick-with.net>
2022-08-01 08:59:04 +0000

    wip

--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
 import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
 import { instantiate } from "./lib/rs_lib.generated.js";
 
-const { add } = await instantiate();
+const { setup, encaps, decaps } = await instantiate();
 
-serve((_req: Request) => new Response(add(1, 2).toString()));
+serve((req: Request) => {
+  const { pathname } = new URL(req.url);
+  if (pathname.startsWith("/setup")) {
+    let pk: Uint8Array, sk: Uint8Array;
+    setup((pk_: Uint8Array, sk_: Uint8Array) => {
+      pk = pk_;
+      sk = sk_;
+      console.log(pk, sk)
+      const ct = encaps(pk, "Hello World");
+    })
+    console.log(pk, sk)
+  }
+  return new Response('setup');
+})
--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -1,33 +1,73 @@
-use ibe::kem::kiltz_vahlis_one::*;
+use ibe::kem::cgw_kv::*;
 use ibe::kem::IBKEM;
+use ibe::Compress;
 use ibe::Derive;
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 
-const ID: &'static str = "email:w.geraedts@sarif.nl";
-pub fn kem() {
-  let mut rng = rand::thread_rng();
+trait JsBind: Compress {
+  fn to_js(&self) -> Uint8Array {
+    Uint8Array::from(self.to_bytes().as_ref())
+  }
+  fn from_js(key: Uint8Array) -> Self {
+    Self::from_bytes(&Self::output(key)).unwrap()
+  }
+  fn output(key: Uint8Array) -> Self::Output;
+}
 
-  // Derive an identity (specific to this scheme).
-  let kid = <KV1 as IBKEM>::Id::derive_str(ID);
-
-  // Generate a public-private-keypair for a trusted third party.
-  let (pk, sk) = KV1::setup(&mut rng);
-
-  // Extract a private key for an identity / user.
-  let usk = KV1::extract_usk(Some(&pk), &sk, &kid, &mut rng);
-
-  // Generate a random message and encrypt it with the public key and an identity.
-  let (c, k) = KV1::encaps(&pk, &kid, &mut rng);
-
-  // Decrypt the ciphertext of that message with the private key of the user.
-  let k2 = KV1::decaps(None, &usk, &c).unwrap();
-
-  assert_eq!(k, k2);
+impl JsBind for PublicKey {
+  fn output(key: Uint8Array) -> Self::Output {
+    key.to_vec().try_into().unwrap()
+  }
+}
+impl JsBind for SecretKey {
+  fn output(key: Uint8Array) -> Self::Output {
+    key.to_vec().try_into().unwrap()
+  }
+}
+impl JsBind for UserSecretKey {
+  fn output(key: Uint8Array) -> Self::Output {
+    key.to_vec().try_into().unwrap()
+  }
+}
+impl JsBind for CipherText {
+  fn output(key: Uint8Array) -> Self::Output {
+    key.to_vec().try_into().unwrap()
+  }
 }
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-  return a + b;
+pub fn setup(f: &js_sys::Function) {
+  let mut rng = rand::thread_rng();
+  let (pk, sk) = CGWKV::setup(&mut rng);
+  f.call2(&JsValue::null(), &pk.to_js(), &sk.to_js()).unwrap();
+}
+
+#[wasm_bindgen]
+pub fn extract_usk(sk_js: Uint8Array, id: &str) -> Uint8Array {
+  let sk = SecretKey::from_js(sk_js);
+  let mut rng = rand::thread_rng();
+  let kid = <CGWKV as IBKEM>::Id::derive(id.as_bytes());
+  let usk = CGWKV::extract_usk(None, &sk, &kid, &mut rng);
+  usk.to_js()
+}
+
+#[wasm_bindgen]
+pub fn encaps(pk_js: Uint8Array, id: &str) -> Uint8Array {
+  let mut rng = rand::thread_rng();
+  let kid = <CGWKV as IBKEM>::Id::derive(id.as_bytes());
+  let (ct, _ss) = CGWKV::encaps(&PublicKey::from_js(pk_js), &kid, &mut rng);
+  ct.to_js()
+}
+#[wasm_bindgen]
+pub fn decaps(usk_js: Uint8Array, ct_js: Uint8Array) -> Uint8Array {
+  let ss = CGWKV::decaps(
+    None,
+    &UserSecretKey::from_js(usk_js),
+    &CipherText::from_js(ct_js),
+  )
+  .unwrap();
+  Uint8Array::from(&ss.0 as &[u8])
 }
 
 #[cfg(test)]
@@ -36,7 +76,7 @@ mod tests {
 
   #[test]
   fn it_works() {
-    let result = add(1, 2);
-    assert_eq!(result, 3);
+    let mut rng = rand::thread_rng();
+    let (pk, sk) = CGWKV::setup(&mut rng);
   }
 }
