8bb63987659b1c3b044f7ae8c986804e5899ef83
Ohkubo KOHEI <kuboon@trick-with.net>
2023-02-02 02:03:27 +0000

    hono

--- server.ts
+++ server.ts
@@ -1,34 +1,37 @@
-import { Handler, serve } from "std/http/server.ts";
+import { serve } from "std/http/server.ts";
+import { Hono } from "https://deno.land/x/hono@v3.0.0-rc.9/mod.ts"
+import { cors } from "https://deno.land/x/hono@v3.0.0-rc.9/middleware.ts"
 import { decode } from "std/encoding/base64.ts";
 import { extract_usk, instantiate } from "./lib/rs_lib.generated.js";
 await instantiate();
 
 const pk = decode(await Deno.readTextFile("master.key"))
 const sk = decode(Deno.env.get("IBKEM_SECRET_KEY")!)
-const handler: Handler = (req) => {
-  const pk_match = new URLPattern({ pathname: "/master_pk" }).exec(req.url);
-  if (pk_match) {
-    return new Response(pk);
+
+const app = new Hono()
+app.use('/', cors({
+  origin: 'https://*.kbn.one',
+}))
+app.get('/master_pk', c => c.body(pk))
+app.get('/at/:at', c => {
+  const at = c.req.param('at')
+  const at_date = Date.parse(at)
+  if (isNaN(at_date)) {
+    c.status(400)
+    return c.text("Invalid date")
   }
-  const at_match = new URLPattern({ pathname: "/at/:at" }).exec(req.url);
-  if (at_match) {
-    const { at } = at_match.pathname.groups;
-    const at_date = Date.parse(at)
-    if (isNaN(at_date)) {
-      return new Response("Invalid date", {status: 400})
-    }
-    if(Date.now() < at_date) {
-      return new Response("It is not the time")
-    } else {
-      const usk = extract_usk(sk, at);
-      return new Response(usk);
-    }
+  if(Date.now() < at_date) {
+    return c.text("It is not the time")
+  } else {
+    const usk = extract_usk(sk, at);
+    return c.body(usk);
   }
-  return new Response(`
+})
+app.get('/', c => {
+  return c.text(`
   Usage:
   /master_pk to get the master public key
   /at/<date> to get the secret key for the given date
-  `);
-};
-
-serve(handler);
+  `)
+})
+serve(app.fetch)
